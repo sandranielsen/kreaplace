b3757a72017ff0526f174d04e802b4d029d103b2
Ana <eaaania@students.eaaa.dk>
2020-05-20 12:40:25 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ js/js.js
@@ -0,0 +1,18 @@
+// dropdown menu - source: https://www.w3schools.com/howto/howto_js_dropdown.asp
+
+function myFunction() {
+     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+     if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+               var openDropdown = dropdowns[i];
+               if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+               }
+          }
+     }
+}
