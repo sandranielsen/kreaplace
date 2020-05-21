b861b9d936a90e00cfce90ac2a209106bf17fd95
Sandra <eaasmmn@students.eaaa.dk>
2020-05-21 12:42:21 +0200

    Forum page adjustments

--- js/forum.js
+++ js/forum.js
@@ -14,3 +14,35 @@ function myButton() {
     moreText.style.display = "inline";
   }
 };
+
+function myButton1() {
+  let dots = document.getElementById("dots1");
+  let moreText = document.getElementById("more1");
+  let btnText = document.getElementById("myButton1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read (5 Answers)";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+};
+
+function myButton2() {
+  let dots = document.getElementById("dots2");
+  let moreText = document.getElementById("more2");
+  let btnText = document.getElementById("myButton2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read (2 Answers)";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+};
