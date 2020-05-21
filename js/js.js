83e3432241480a00f51f32af745cf24bdb6435fe
Sandra <eaasmmn@students.eaaa.dk>
2020-05-21 12:00:11 +0200

    Forum adjustments

--- js/js.js
+++ js/js.js
@@ -16,3 +16,20 @@ window.onclick = function (event) {
           }
      }
 }
+
+// read answers - source: https://www.w3schools.com/howto/howto_js_read_more.asp
+function myFunction() {
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("myButton");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read (3 Answers)";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+};
