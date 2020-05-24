ca0af99a143bef9f1a2a7f494b810e271999b2fa
Sona <sona.juhasova@gmail.com>
2020-05-24 12:16:03 +0200

    js fp

---
+++ js/frontpage.js
@@ -0,0 +1,15 @@
+let childA = document.querySelector('#loginFP');
+let childB = document.querySelector('#signinFP');
+
+function myFunctionFP(element) {
+    if (childA.classList.contains('front'));
+    console.log('i have class');
+}
+console.log('i dont have');
+
+
+function myFunctionFP1(element) {
+    if (childA.classList.contains('front'));
+    console.log('i have class too');
+}
+console.log('i dont have too');
