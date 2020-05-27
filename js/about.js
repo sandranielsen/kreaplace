d4d52ed36c0f272b5e0e09cbe8b3a291979c5343
Ana <eaaania@students.eaaa.dk>
2020-05-27 14:39:13 +0200

    about page animation v2

--- js/about.js
+++ js/about.js
@@ -1,26 +1,10 @@
-let section1 = document.getElementById("section1");
 let section2 = document.getElementById("section2");
 
-if (section1.getBoundingClientRect().bottom < window.innerHeight) {
-     section1.style.opacity = 1;
-}
-
-if (section1.getBoundingClientRect().top >= 0) {
-     section1.style.opacity = 0;
-}
-
-window.addEventListener("scroll", function () {
-     if (elVisible(section1)) {
-          section1.style = "animation: fadein 2s";
-          console.log("Is this working?");
-     }
-});
-
-if (section1.getBoundingClientRect().bottom < window.innerHeight) {
+if (section2.getBoundingClientRect().bottom < window.innerHeight) {
      section2.style.opacity = 1;
 }
 
-if (section1.getBoundingClientRect().top >= 0) {
+if (section2.getBoundingClientRect().top >= 0) {
      section2.style.opacity = 0;
 }
 
@@ -30,7 +14,6 @@ window.addEventListener("scroll", function () {
           console.log("Is this working?");
      }
 });
-
 
 function elVisible(el) {
      return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
